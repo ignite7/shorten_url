7091a9d777c7710ac45e711d89b85ad325ed2aeb
Sergio van Berkel <sergio@settly.nl>
2024-12-19 21:52:45 +0100

    fix hrm using exposing port

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
-import vue from '@vitejs/plugin-vue';
-import laravel from 'laravel-vite-plugin';
-import { defineConfig } from 'vite';
-import checker from 'vite-plugin-checker';
+import vue from "@vitejs/plugin-vue";
+import laravel from "laravel-vite-plugin";
+import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
 
 export default defineConfig({
+  server: {
+    host: true,
+    port: 3000,
+    strictPort: true,
+    hmr: {
+      host: "localhost",
+    },
+  },
   plugins: [
     laravel({
-      input: 'resources/js/app.ts',
-      ssr: 'resources/js/ssr.ts',
+      input: "resources/js/app.ts",
+      ssr: "resources/js/ssr.ts",
       refresh: true,
     }),
     vue({
