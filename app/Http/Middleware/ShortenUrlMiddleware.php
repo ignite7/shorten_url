57616d8b04950bda245d6848d9f52d18116f3610
Sergio van Berkel <sergio@settly.nl>
2025-01-13 16:51:52 +0100

    code fixer

--- app/Http/Middleware/ShortenUrlMiddleware.php
+++ app/Http/Middleware/ShortenUrlMiddleware.php
@@ -12,16 +12,16 @@ class ShortenUrlMiddleware
     /**
      * Handle an incoming request.
      *
-     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (!$ip = $request->ip()) {
+        if (! $ip = $request->ip()) {
             return response(status: Response::HTTP_BAD_REQUEST);
         }
 
         // Limit the number of requests per IP address to 5 per day if the user is not authenticated.
-        if (!$request->user()) {
+        if (! $request->user()) {
             $requests = RequestModel::query()
                 ->where('ip_address', $ip)
                 ->whereDate('created_at', now())
