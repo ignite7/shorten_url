090464d20c32e775002e51b5dce7d2d6e34d09e4
Sergio van Berkel <sergio@settly.nl>
2025-01-15 18:48:55 +0100

    add debugbar package

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\SoloServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
+    Barryvdh\Debugbar\ServiceProvider::class,
 ];
