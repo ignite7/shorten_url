0954c7a0341f20d86fba70a388b8fad6eb90f905
Sergio van Berkel <sergio@settly.nl>
2025-01-20 01:30:48 +0100

    chore: update githooks test 7

--- entrypoints/git-hooks.sh
+++ entrypoints/git-hooks.sh
@@ -5,3 +5,16 @@ set -o nounset
 
 # Copy git hooks
 cp -r githooks/* .git/hooks
+
+printf "Moving git hooks to '.git/hooks' 📁\n"
+
+# Make only the copied files executable
+for file in githooks/*; do
+    if [ -f "$file" ]; then
+        target=".git/hooks/$(basename "$file")"
+        chmod +x "$target"
+        printf "Made %s git hook executable 📄\n" "$target"
+    fi
+done
+
+printf "Git hooks copied and made executable! ✅\n"
