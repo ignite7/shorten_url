f1c1d925a301e5535226f1d8b023a0238ef3d2fd
Sergio van Berkel <sergio@settly.nl>
2025-01-29 01:01:31 +0100

    style: add button ui in header

--- resources/js/components/Header/index.tsx
+++ resources/js/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import { Button } from '@/components/ui/button';
+import { LogIn } from 'lucide-react';
 import styles from './index.module.css';
 
 export default function Header() {
@@ -5,8 +7,10 @@ export default function Header() {
     <header className={styles.header}>
       <div className={styles.logo}>ShortenURL</div>
       <div className={styles.buttons}>
-        <button>Login</button>
-        <button>Register</button>
+        <Button variant={'secondary'}>
+          Login <LogIn />
+        </Button>
+        <Button>Register</Button>
       </div>
     </header>
   );
