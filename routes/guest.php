52cd4276d920e87c763e3807bd27031de6002ff1
Sergio van Berkel <sergio@settly.nl>
2025-02-01 01:19:51 +0100

    chore: moved route

--- routes/guest.php
+++ routes/guest.php
@@ -3,7 +3,12 @@
 declare(strict_types=1);
 
 use App\Actions\Urls\ShortenUrl;
+use App\Http\Middleware\AnonymousTokenMiddleware;
 use Illuminate\Support\Facades\Route;
+
+Route::middleware(AnonymousTokenMiddleware::class)
+    ->get('/', static fn () => inertia('Home'))
+    ->name('home');
 
 Route::name('urls.')
     ->prefix('urls')
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,4 @@
 
 declare(strict_types=1);
 
-use App\Http\Middleware\AnonymousTokenMiddleware;
-use Illuminate\Support\Facades\Route;
-
-Route::middleware(AnonymousTokenMiddleware::class)
-    ->get('/', static fn () => inertia('Home'))
-    ->name('home');
-
-require __DIR__.'/guest.php';
+require __DIR__ . '/guest.php';
